2b019108ba19adc5acacf0faf71e73db56aae32e
superdev905 <superdev905@gmail.com>
2021-09-08 08:30:17 -0500

    adding test docker compose file

--- app/settings.py
+++ app/settings.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 ENV = os.getenv("ENV")
 DATABASE_URL = os.getenv("DATABASE_URL")
-EMPLOYEE_SERVICE = 'http://823e-190-236-251-178.ngrok.io' if ENV == "development" else 'http://fcchc-itprocess.southcentralus.cloudapp.azure.com:5104'
-PARAMETERS_SERVICE = 'http://10e1-190-236-251-178.ngrok.io' if ENV == "development" else 'http://fcchc-itprocess.southcentralus.cloudapp.azure.com:5105'
+EMPLOYEE_SERVICE = 'http://058a-190-236-251-178.ngrok.io' if ENV == "development" else 'http://fcchc-itprocess.southcentralus.cloudapp.azure.com:5104'
+PARAMETERS_SERVICE = 'http://2ea6-190-236-251-178.ngrok.io' if ENV == "development" else 'http://fcchc-itprocess.southcentralus.cloudapp.azure.com:5105'
