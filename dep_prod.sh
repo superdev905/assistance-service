fe62ec02bd9d59339c00f12877efbe5f464d815b
superdev905 <superdev905@gmail.com>
2022-03-04 02:20:37 -0300

    dep_prod.sh

--- dep_prod.sh
+++ dep_prod.sh
@@ -1,8 +1,8 @@
 #/bin/bash!
+cp .env_prod .env
 docker context use default
-docker-compose --file docker-compose.test.yml up --build
-docker tag 
+docker-compose --file docker-compose.image.yml up --build
 docker tag assistance-service-prod_assistance-api cchcprod.azurecr.io/assistance-prod:latest
 docker push cchcprod.azurecr.io/assistance-prod:latest
-docker context use azurecontext
-docker compose --file docker-compose.prod.yml up --build 
+docker context use azureprod
+docker compose --file docker-compose.prod.yml up --build
