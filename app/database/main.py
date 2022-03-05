b00c5db73bd0986aaacbaa593f6f7b067c3d3cde
superdev905 <superdev905@gmail.com>
2022-03-05 01:24:33 -0300

    PoolSize

--- app/database/main.py
+++ app/database/main.py
@@ -4,16 +4,15 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.settings import DATABASE_URL
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True, pool_size=60)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True, pool_size=32)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
 
 def get_database() -> Generator:
-    db = SessionLocal()
-
     try:
+        db = SessionLocal()
         yield db
     finally:
         db.close()
