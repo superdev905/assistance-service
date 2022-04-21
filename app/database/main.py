f7bd77f74bbccc467e16b77973b398f5338ff596
superdev905 <superdev905@gmail.com>
2022-04-20 23:33:40 -0400

    closedb

--- app/database/main.py
+++ app/database/main.py
@@ -4,15 +4,20 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.settings import DATABASE_URL
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True, pool_size=32)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True, pool_size=32, max_overflow=32, isolation_level="AUTOCOMMIT")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
 
 def get_database() -> Generator:
+    db = SessionLocal()
+
     try:
-        db = SessionLocal()
         yield db
+    except:
+        db.rollback()
+        raise
     finally:
         db.close()
+        engine.dispose()
